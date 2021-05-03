79ee98385fd294af32c063b4302cde4357e48860
clarababette <clara_coetzee@zoho.com>
2021-05-03 10:29:53 +0200

    actioned feedback

--- js/matching_days_DOM.js
+++ js/matching_days_DOM.js
@@ -1,43 +1,57 @@
-var date1 = document.querySelector("#date1");
-var date2 = document.querySelector("#date2");
-var dayElems = document.querySelectorAll(".day");
-var theseDays = matchingDays();
+var matchDays = {
+  day: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+  date: {
+    date1: "First date",
+    date2: "Second date",
+  },
+};
 
-date1.addEventListener("change", function () {
-  dayClassesClear();
-  theseDays.setDate1(date1.value);
-  dayClasses();
-});
+document.addEventListener("DOMContentLoaded", function () {
+  var templateSource = document.querySelector("#matching_days_template").innerHTML;
+  var matchingDaysTemplate = Handlebars.compile(templateSource);
+  var matchingDaysHTML = document.querySelector("#matching_days");
+  matchingDaysHTML.innerHTML = matchingDaysTemplate(matchDays);
+  var date1 = document.querySelector("#date1");
+  var date2 = document.querySelector("#date2");
+  var dayElems = document.querySelectorAll(".day");
+  var theseDays = matchingDays();
 
-date2.addEventListener("change", function () {
-  dayClassesClear();
-  theseDays.setDate2(date2.value);
-  dayClasses();
-});
+  date1.addEventListener("change", function () {
+    dayClassesClear();
+    theseDays.setDate1(date1.value);
+    dayClasses();
+  });
 
-function dayClasses() {
-  if (theseDays.sameDayCheck()) {
-    document.getElementById(theseDays.getDay1()).classList.add("sameday");
-  } else {
-    if (date1.value) {
-      document.getElementById(theseDays.getDay1()).classList.add("date1day");
-    }
-    if (date2.value) {
-      document.getElementById(theseDays.getDay2()).classList.add("date2day");
+  date2.addEventListener("change", function () {
+    dayClassesClear();
+    theseDays.setDate2(date2.value);
+    dayClasses();
+  });
+
+  function dayClasses() {
+    if (theseDays.sameDayCheck()) {
+      document.getElementById(theseDays.getDay1()).classList.add("sameday");
+    } else {
+      if (date1.value) {
+        document.getElementById(theseDays.getDay1()).classList.add("date1day");
+      }
+      if (date2.value) {
+        document.getElementById(theseDays.getDay2()).classList.add("date2day");
+      }
     }
   }
-}
 
-function dayClassesClear() {
-  dayElems.forEach(function (day) {
-    if (day.classList.contains("date1day")) {
-      day.classList.remove("date1day");
-    }
-    if (day.classList.contains("date2day")) {
-      day.classList.remove("date2day");
-    }
-    if (day.classList.contains("sameday")) {
-      day.classList.remove("sameday");
-    }
-  });
-}
+  function dayClassesClear() {
+    dayElems.forEach(function (day) {
+      if (day.classList.contains("date1day")) {
+        day.classList.remove("date1day");
+      }
+      if (day.classList.contains("date2day")) {
+        day.classList.remove("date2day");
+      }
+      if (day.classList.contains("sameday")) {
+        day.classList.remove("sameday");
+      }
+    });
+  }
+});
